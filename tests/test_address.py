a9c5e987fa8a2ad9c63ac897253ffe8a214b6313
wakamezake <hotwater1367@gmail.com>
2020-08-12 10:15:24 +0900

    [update] add test

--- tests/test_address.py
+++ tests/test_address.py
@@ -1,3 +1,4 @@
+import pytest
 from dataclasses import is_dataclass
 
 from japanese_addresses import separate_address, ParsedAddress
@@ -21,3 +22,22 @@ def test_separate_address():
     parsed_address = separate_address('京東都')
     assert is_dataclass_instance(parsed_address)
 
+
+test_data = [
+    pytest.param('北海道余市郡余市町朝日町', ('北海道', '余市郡余市町'),
+                 marks=pytest.mark.xfail),
+    ('宮城県仙台市泉区市名坂字東裏97-1', ('宮城県', '仙台市泉区')),
+    ('奈良県高市郡高取町', ('奈良県', '高市郡高取町')),
+    pytest.param('北海道余市郡余市町黒川町', ('北海道', '余市郡余市町'),
+                 marks=pytest.mark.xfail),
+    ('東京都', ('東京都', '')),
+    ('台東区', ('', '')),
+]
+
+
+@pytest.mark.parametrize('address, expect', test_data)
+def test_separate_japanese_addresses(address, expect):
+    prefecture, city = expect
+    parsed_address = separate_address(address)
+    assert parsed_address.prefecture == prefecture
+    assert parsed_address.city == city
